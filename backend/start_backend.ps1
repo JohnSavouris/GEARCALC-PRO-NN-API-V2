fe71222f147d31a029c63fdfa8788b678add02d0
JohnSavouris <145044416+JohnSavouris@users.noreply.github.com>
2026-02-25 19:20:52 +0200

    Delete backend directory

--- backend/start_backend.ps1
+++
@@ -1,5 +0,0 @@
-# Run from backend/ folder
-# Optional MATLAB mode:
-#   $env:USE_MATLAB="1"
-#   $env:MATLAB_EXE="C:\Program Files\MATLAB\R2024b\bin\matlab.exe"
-python -m uvicorn api_server:app --reload --host 127.0.0.1 --port 8000
